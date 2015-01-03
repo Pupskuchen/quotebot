85300223e74743293dfd9334895ef8c9691ce163
Pupskuchen <pkn@thepups.net>
2015-01-03 14:57:07 +0100

    add usage module

---
+++ modules/usage.js
@@ -0,0 +1,25 @@
+exports.command = "usage"; // the command
+
+exports.desc = {
+	params: "", // parameters (shown in help)
+	desc: "prints memory usage", // command description (shown in help)
+	needed: "allowed" // needed permissions, can be an empty string, "allowed" or "owner" (used for help)
+};
+
+function ByteToMB(b) {
+	return (b/1024/1024).toPrecision(4);
+}
+
+// to be executed on command
+exports.exec = function() {
+	var mU = process.memoryUsage();
+	chanMsg('rss: '+ByteToMB(mU.rss)+' MB, '+'heap total: '+ByteToMB(mU.heapTotal)+' MB, heap used: '+ByteToMB(mU.heapUsed)+' MB');
+	mU = null;
+};
+
+/*
+// "destructor"
+exports.stop = function() {
+
+};
+*/
