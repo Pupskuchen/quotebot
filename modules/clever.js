0a79f3620741fd0e9be2de76781bc578555d46ac
Pupskuchen <pkn@thepups.net>
2015-02-24 13:23:53 +0100

    add cleverbot

---
+++ modules/clever.js
@@ -0,0 +1,22 @@
+var Cleverbot = require('cleverbot-node'),
+    Entities  = require('html-entities').AllHtmlEntities;
+
+
+function handler(e) {
+	if (e.message.toLowerCase().indexOf(client.me.getNick().toLowerCase() + ':') === 0) {
+		var cb = new Cleverbot(),
+		entities = new Entities();
+		var text = e.message.substr((client.me + ':').length).trim();
+		cb.write(text, function (response) {
+      setTimeout(function () {
+        client.send(e.channel.getName(), e.user.getNick()+": "+entities.decode(response.message));
+      }, 7000);
+		});
+		}
+	}
+
+client.on('message', handler);
+
+exports.kill = function() {
+  client.removeListener('message', handler);
+};
