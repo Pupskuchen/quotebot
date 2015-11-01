62d7e5836ddaad48b9e74b5c587c9ff013a58825
albert-- <albert--@users.noreply.github.com>
2015-11-01 02:21:26 +0100

    code style

--- modules/topicVar.js
+++ modules/topicVar.js
@@ -9,9 +9,9 @@ exports.desc = {
 var self = this;
 c.tvar_allowedModes = c.tvar_allowedModes || "%@~";
 
-exports.exec = function() {
-	if(isAllowed()) {
-		if(paramstringparam.length < 1 && params.length < 1) return paramError("tvar <var> <value>");
+exports.exec = function () {
+	if (isAllowed()) {
+		if (paramstringparam.length < 1 && params.length < 1) return paramError("tvar <var> <value>");
 		paramstringparam = paramstringparam === "++" || paramstringparam === "--" ? paramstringparam : paramstringparam.replace(/[^\w :\/.äüöÄÜÖß\d]/g, '');
 		params[0] = params[0].replace(/[^\w\d-äöüÄÖÜß.]/g, '');
 		var chan = this.chan;
@@ -19,26 +19,26 @@ exports.exec = function() {
 		var regex = /\s*([\w\d-äöüÄÖÜß.]+):\s+([\w :\/.äüöÄÜÖß\d]+)/g;
 		var match = regex.exec(topic);
 		var vars = {};
-		while(match != null) {
+		while (match != null) {
 			vars[match[1]] = match[2].trim();
 			match = regex.exec(topic);
 		}
-		if(stringparam in vars) return chanMsg(stringparam+" = "+vars[stringparam]);
-		else if(params[0] !== "unset" && paramstringparam.length < 1) {
-			if(params[0] in vars) return chanMsg(params[0]+" = "+vars[params[0]]);
+		if (stringparam in vars) return chanMsg(stringparam+" = "+vars[stringparam]);
+		else if (params[0] !== "unset" && paramstringparam.length < 1) {
+			if (params[0] in vars) return chanMsg(params[0]+" = "+vars[params[0]]);
 			else return chanMsg("no such topic variable");
 		}
 		else {
-			if(params[0] === "unset") { // unset
-				if(paramstringparam.length < 1) return paramError("tvar unset <var>");
-				else if(!(paramstringparam in vars)) return chanMsg("no such topic variable");
+			if (params[0] === "unset") { // unset
+				if (paramstringparam.length < 1) return paramError("tvar unset <var>");
+				else if (!(paramstringparam in vars)) return chanMsg("no such topic variable");
 				var reg = new RegExp("[\\s\\\|]+"+paramstringparam+":\\s+"+vars[paramstringparam], "g");
 				client.topic(chan, topic.replace(reg, ''));
 				return;
 			}
-			if(params[0] in vars) { // change
-				if(paramstringparam === "++" || paramstringparam === "--") {
-					if(isNaN(vars[params[0]])) return chanMsg("you cannot do that, silly");
+			if (params[0] in vars) { // change
+				if (paramstringparam === "++" || paramstringparam === "--") {
+					if (isNaN(vars[params[0]])) return chanMsg("you cannot do that, silly");
 				}
 				client.topic(chan, topic.replace(params[0]+": "+vars[params[0]], params[0]+": "+(paramstringparam === "++" ? (parseInt(vars[params[0]])+1) : (paramstringparam === "--" ? (parseInt(vars[params[0]])-1) : paramstringparam))));
 			} else { // create
@@ -48,9 +48,9 @@ exports.exec = function() {
 	} else return permissionError();
 };
 
-function isAllowed() {
+function isAllowed () {
 	for(var i = 0; i < c.tvar_allowedModes.length; i++) {
-		if(self.user.channels[self.chan].indexOf(c.tvar_allowedModes[i]) != -1) return true;
+		if (self.user.channels[self.chan].indexOf(c.tvar_allowedModes[i]) != -1) return true;
 	}
 	return false;
 }
